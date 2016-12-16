9128f876a76b3773867c9115cbba7b4af0053c84
Andrew Hunt <andrew@aghstrategies.com>
2016-12-16 12:30:02 -0500

    rewrite using modern techniques

---
+++ js/popemployer.js
@@ -0,0 +1,53 @@
+CRM.$(function($) {
+
+  var employerEdit = function() {
+    $employer.show();
+    $clickToEdit.detach();
+  };
+
+  if( $('input#current_employer').length > 0) {
+    var $employer = $('input#current_employer');
+    var $clickToEdit = $('<span></span>', {
+      class: "popemployer-hidden-edit",
+      style: "cursor: pointer",
+    });
+    $clickToEdit.click(employerEdit);
+
+    var waitToSearch = setTimeout(function() {
+      runSearch($('#crm-container input[name^="email-"]').val());
+    }, 750);
+    $('#crm-container input[name^="email-"]').each( function() {
+      $(this).change( function() {
+        prepSearch($(this).val());
+      });
+      $(this).keyup( function() {
+        prepSearch($(this).val());
+      });
+    });
+  }
+
+  function prepSearch(email) {
+    clearTimeout(waitToSearch);
+    waitToSearch = setTimeout(function() {
+      runSearch(email);
+    }, 750);
+  }
+
+  function runSearch(email) {
+    if (email.search("@") >= 0 && email.lastIndexOf(".") > email.search("@")) {
+      CRM.api3('Popemployer', 'get', {
+        "email": email,
+      }).done(function(result) {
+        if (result.result === true) {
+          $employer.show();
+          $clickToEdit.detach();
+        }
+        else {
+          $employer.val(result.result);
+          $clickToEdit.text(result.result+' (click to edit)');
+          $employer.hide().after($clickToEdit);
+        }
+      });
+    }
+  }
+});
